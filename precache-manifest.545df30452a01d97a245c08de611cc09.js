73339b623c4c95033fea1c37f587a233d922cf16
TURKCELL\TCUTANER <ugur.taner@turkcell.com.tr>
2021-12-13 12:01:58 +0300

    Updates

---
+++ precache-manifest.545df30452a01d97a245c08de611cc09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5685c74fb3f8a67cf27",
+    "url": "/myreads.git/static/css/main.9e62d209.chunk.css"
+  },
+  {
+    "revision": "a5685c74fb3f8a67cf27",
+    "url": "/myreads.git/static/js/main.a5685c74.chunk.js"
+  },
+  {
+    "revision": "d660a0dbbba7d787a836",
+    "url": "/myreads.git/static/js/1.d660a0db.chunk.js"
+  },
+  {
+    "revision": "1e839e61f4611068117b",
+    "url": "/myreads.git/static/js/runtime~main.1e839e61.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads.git/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads.git/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads.git/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "b1319ca2263c67418ca3487266004508",
+    "url": "/myreads.git/index.html"
+  }
+];
